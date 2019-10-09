ca865439df561f89db92627558c4c84e9fa0c00a
ruixuanHe <ruixuanheweb@gmail.com>
2019-10-08 22:13:30 -0400

    ex3

--- W5/ex3Js.js
+++ W5/ex3Js.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
-  $("#getInput").on("click", function() {
+  $("#getInput").click(function() {
     console.log("clicked");
     let searchVal = $('#inputSearchVal').val();
-    if (searchVal == "My Days")
-      displayDayDetail();
+    searchVal = searchVal.toLocaleLowerCase();
+    if (searchVal == "day" ||
+      searchVal == "transportation" ||
+      searchVal == "breakfast" ||
+      searchVal == "lunch" ||
+      searchVal == "dinner" ||
+      searchVal == "clothing" ||
+      searchVal == "exercise")
+      displayDayDetail(searchVal);
     else
       displayNoData();
   });
 });
 
-function displayDayDetail() {
+
+function displayDayDetail(searchVal) {
   var jqxhr = $.getJSON("dayDetail.json", function() {
     console.log("success");
     console.log(jqxhr.responseJSON);
@@ -17,19 +25,21 @@ function displayDayDetail() {
     for (var day in jsonObject) {
       if ($('#' + day).length) {
         $('#searchResultDiv').empty();
+        $('#searchResultDiv').append("<p>Enter day/transportation/breakfast/lunch/dinner/clothing/exercise</p>");
         $('#' + day).empty();
         $('#' + day).append("<h1>" + day + "</h1>")
         var dayDetail = jsonObject[day][0];
         for (var prop in dayDetail) {
-          $('#' + day).append("<p>" + prop + " : " + dayDetail[prop] + "</p>")
+          if (prop == searchVal || searchVal == "day")
+            $('#' + day).append("<p>" + prop + " : " + dayDetail[prop] + "</p>")
         }
       }
     }
   });
-
 }
 
 function displayNoData() {
   $('#searchResultDiv').empty();
+  $('.dayDiv').empty();
   $('#searchResultDiv').append("<p>No Data Found</p>");
 }
